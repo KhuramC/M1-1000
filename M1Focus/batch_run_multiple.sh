b763ffc6ce876853358c5ab31d8cecd45092ff31
KhuramC <khuramchoudhry123@gmail.com>
2024-02-17 15:42:47 -0600

    updated how to run multiple jobs.

--- M1Focus/batch_run_multiple.sh
+++ M1Focus/batch_run_multiple.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 
-# Usage: RUNS=10 ./batch_run_multiple.sh 
-# Will kick off 10 runs for each 
+# Usage: START=1 RUNS=10 ./batch_run_multiple.sh 
+# Will kick off 10 runs for each starting at #1.
 
-
+START="${START:-1}"
 
 RUNS="${RUNS:-1}"
 
-for ((i = 1 ; i <= $RUNS ; i++)); do
+for ((i = $START ; i < $RUNS+$START ; i++)); do
 	RUN_NUM=$i ./batch_all.sh
 done
+
+
