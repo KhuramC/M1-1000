a5a68443ddc7974882cdbb1c3b2d38af901bec2d
KhuramC <khuramchoudhry123@gmail.com>
2024-03-01 10:47:16 -0600

    simv6 run 16
    decreased LTS gap junction conductance

--- M1Focus/batch_all.sh
+++ M1Focus/batch_all.sh
@@ -6,9 +6,9 @@
 RUN_NUM="${RUN_NUM:-1}"
 echo $RUN_NUM
 
-OUTPUT_DIR=../Analysis/simulation_results_v4/baseline/baseline_$RUN_NUM sbatch batch_baseline.sh
-OUTPUT_DIR=../Analysis/simulation_results_v4/short/short_$RUN_NUM sbatch batch_short.sh
-OUTPUT_DIR=../Analysis/simulation_results_v4/long/long_$RUN_NUM sbatch batch_long.sh
+OUTPUT_DIR=../Analysis/simulation_results_v6/baseline/baseline_$RUN_NUM sbatch batch_baseline.sh
+OUTPUT_DIR=../Analysis/simulation_results_v6/short/short_$RUN_NUM sbatch batch_short.sh
+OUTPUT_DIR=../Analysis/simulation_results_v6/long/long_$RUN_NUM sbatch batch_long.sh
 #OUTPUT_DIR=./outout/baseline/run$RUN_NUM sbatch batch_baseline.sh
 #OUTPUT_DIR=./outout/short/run$RUN_NUM sbatch batch_short.sh
 #OUTPUT_DIR=./outout/long/run$RUN_NUM sbatch batch_long.sh
